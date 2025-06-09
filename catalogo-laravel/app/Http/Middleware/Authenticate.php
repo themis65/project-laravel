b6171177c7b69eeea1315bc4ff16e559c57d0ad2
themiss65 <btaco3565@gmail.com>
2025-06-08 22:47:56 -0500

    Se crea Authenticate.php para habilitar el endpoint logout

---
+++ catalogo-laravel/app/Http/Middleware/Authenticate.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
+
+class Authenticate extends Middleware
+{
+    protected function redirectTo($request)
+    {
+        if (!$request->expectsJson()) {
+            abort(response()->json(['message' => 'No autenticado.'], 401));
+        }
+    }
+}
--- catalogo-laravel/app/Http/Middleware/IsAdmin.php
+++ catalogo-laravel/app/Http/Middleware/IsAdmin.php
@@ -8,9 +8,13 @@ class IsAdmin
 {
     public function handle(Request $request, Closure $next)
     {
-        if ($request->user() && $request->user()->rol === 'admin') {
-            return $next($request);
+        $user = $request->user();
+        if (!$user) {
+            return response()->json(['message' => 'No autenticado'], 401);
         }
-        return response()->json(['message' => 'No autorizado'], 403);
+        if ($user->rol !== 'admin') {
+            return response()->json(['message' => 'No autorizado'], 403);
+        }
+        return $next($request);
     }
 }
